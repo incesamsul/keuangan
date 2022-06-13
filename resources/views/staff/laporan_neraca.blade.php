cb58d5ad5e0d24346d9f368d40e59f2ae97b0146
incesamsul <incesamsul89@gmail.com>
2022-06-13 15:36:52 +0800

    fix laporan neraca

--- resources/views/staff/laporan_neraca.blade.php
+++ resources/views/staff/laporan_neraca.blade.php
@@ -195,11 +195,12 @@
         </div>
     </div>
 
+    {{-- AKTIVA  --}}
     <div class="row">
         <div class="col-lg-12">
             <div class="card ">
                 <div class="card-header d-flex justify-content-between">
-                    <h4>Laporan Neraca</h4>
+                    <h4>Laporan Neraca (Aktiva)</h4>
                     <a href="{{ URL::to('/staff/cetak_neraca') }}" target="_blank" class="btn btn-warning mt-3">
                         <i class="fas fa-print"></i> Cetak
                     </a>
@@ -218,7 +219,7 @@
                             <?php $totalDebit = 0 ?>
                             <?php $totalKredit = 0 ?>
                             @foreach ($akun as $item)
-                            @if (substr($item->no_akun,0,1) == 1 || substr($item->no_akun,0,1) == 2 || substr($item->no_akun,0,1) == 3)
+                            @if (substr($item->no_akun,0,1) == 1)
                             <tr>
                                 <td>{{ $item->no_akun }}</td>
                                 <td>{{ $item->nama_akun }}</td>
@@ -254,9 +255,82 @@
                             @endif
                             @endforeach
                             <tr>
-                                <th colspan="2" class="text-center">TOTAL</th>
+                                <th colspan="2" class="text-center">TOTAL AKTIVA</th>
                                 <th class="text-center">Rp.
                                     {{ number_format($totalDebit) }}
+                                </th>
+                                <th class="text-center">
+                                    {{-- {{ number_format($totalKredit) }} --}}
+                                </th>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    {{-- PASSIVA --}}
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card ">
+                <div class="card-header d-flex justify-content-between">
+                    <h4>Laporan Neraca (passiva)</h4>
+                </div>
+                <div class="card-body">
+                    <table id="perUser" class="table table-striped table-bordered" style="width:100%">
+                        <thead>
+                            <tr>
+                                <td>No Akun</td>
+                                <td>Nama Akun</td>
+                                <td>Debit</td>
+                                <td>Kredit</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php $totalDebit = 0 ?>
+                            <?php $totalKredit = 0 ?>
+                            @foreach ($akun as $item)
+                            @if (substr($item->no_akun,0,1) == 2 || substr($item->no_akun,0,1) == 3)
+                            <tr>
+                                <td>{{ $item->no_akun }}</td>
+                                <td>{{ $item->nama_akun }}</td>
+                                <td>
+                                    @if (substr($item->no_akun,0,1) == 6)
+                                        {{ 'Rp. ' .number_format(App\Models\Jurnal::all()->where('id_akun', $item->id_akun)->sum('debit') - App\Models\Jurnal::all()->where('id_akun', $item->id_akun)->sum('kredit')) }}
+                                        <?php
+                                            $totalDebit += App\Models\Jurnal::all()->where('id_akun', $item->id_akun)->sum('debit') - App\Models\Jurnal::all()->where('id_akun', $item->id_akun)->sum('kredit');
+                                            ?>
+                                    @else
+                                        Rp. ----
+                                    @endif
+                                </td>
+                                <td>
+                                    @if ($item->nama_akun == "Modal Tn. A")
+                                        Rp. {{ number_format($laporanModal) }}
+                                        <?php
+                                        $totalKredit += $laporanModal;
+                                         ?>
+                                    @else
+                                    @if (substr($item->no_akun,0,1) == 2 || substr($item->no_akun,0,1) == 3 || substr($item->no_akun,0,1) == 4)
+                                        {{ 'Rp. ' .number_format(App\Models\Jurnal::all()->where('id_akun', $item->id_akun)->sum('kredit') - App\Models\Jurnal::all()->where('id_akun', $item->id_akun)->sum('debit')) }}
+                                            <?php
+                                            $totalKredit += App\Models\Jurnal::all()->where('id_akun', $item->id_akun)->sum('kredit') - App\Models\Jurnal::all()->where('id_akun', $item->id_akun)->sum('debit');
+                                            ?>
+                                    @else
+                                        Rp. ----
+                                    @endif
+                                    @endif
+                                </td>
+
+                            </tr>
+                            @endif
+                            @endforeach
+                            <tr>
+                                <th colspan="2" class="text-center">TOTAL PASSIVA</th>
+                                <th class="text-center">
+                                    {{-- {{ number_format($totalDebit) }} --}}
                                 </th>
                                 <th class="text-center">Rp.
                                     {{ number_format($totalKredit) }}
